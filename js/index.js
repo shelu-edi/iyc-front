d41a9dec35a502032a44ae22d9a1213e491d0629
Shelumiel Ediriarachchi <61077989+shelu-edi@users.noreply.github.com>
2022-12-30 01:26:01 +0530

    12/20/22

--- js/index.js
+++ js/index.js
@@ -3,6 +3,55 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo')
 
+// Nav Dropdown
+
+const brief = document.getElementById("iyc-brief");
+const worldwide = document.getElementById("iyc-worldwide");
+const country = document.getElementById("iyc-country");
+const affiliations = document.getElementById("iyc-affiliations");
+const agenda = document.getElementById("iyc-agenda");
+
+function Brief() {
+    brief.style.display = 'flex';
+    worldwide.style.display = 'none';
+    country.style.display = 'none';
+    affiliations.style.display = 'none';
+    agenda.style.display = 'none';
+}
+
+function Worldwide() {
+    worldwide.style.display = 'flex';
+    brief.style.display = 'none';
+    country.style.display = 'none';
+    affiliations.style.display = 'none';
+    agenda.style.display = 'none';
+}
+
+function Country() {
+    country.style.display = 'flex';
+    brief.style.display = 'none';
+    worldwide.style.display = 'none';
+    affiliations.style.display = 'none';
+    agenda.style.display = 'none';
+}
+
+function Affiliations() {
+    affiliations.style.display = 'flex';
+    brief.style.display = 'none';
+    worldwide.style.display = 'none';
+    country.style.display = 'none';
+    agenda.style.display = 'none';
+}
+
+function Agenda() {
+    agenda.style.display = 'flex';
+    brief.style.display = 'none';
+    worldwide.style.display = 'none';
+    country.style.display = 'none';
+    affiliations.style.display = 'none';
+}
+
+
 // Display mobile menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active');
@@ -57,5 +106,6 @@ $(document).ready(function() {
         }
     });
 });
+
 
 
