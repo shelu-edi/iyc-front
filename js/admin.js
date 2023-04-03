38368f75763cef112c14bd2cfdf02699f39a694f
Shelumiel Ediriarachchi <61077989+shelu-edi@users.noreply.github.com>
2023-04-03 18:33:25 +0530

    3/4/23

--- js/admin.js
+++ js/admin.js
@@ -4,8 +4,15 @@ const delBtn = document.getElementById('del-btn');
 const closeIcon = document.getElementById('close-icon');
 const editForm = document.getElementById('edit-form');
 
+
+const delContainer = document.getElementById('del-container');
+
+
 const newsWrapper = document.getElementById('news-wrapper');
 const initWrapper = document.getElementById('initiative-wrapper');
+
+const newsHead = document.getElementById('news-head');
+const initHead = document.getElementById('initiative-head');
 
 
 function OpenForm() {
@@ -18,17 +25,46 @@ function CloseForm() {
 }
 
 function OpenInit() {
-    newsWrapper.style.display = 'none';
     initWrapper.style.display = 'flex';
+    newsWrapper.style.display = 'none';
 
-    newsWrapper.classList.remove("active");
-    initWrapper.classList.add("active");
+    newsHead.classList.remove("active");
+    initHead.classList.add("active");
 }
 
 function OpenNews() {
     initWrapper.style.display = 'none';
     newsWrapper.style.display = 'flex';
 
-    initWrapper.classList.remove("active");
-    newsWrapper.classList.add("active");
+    initHead.classList.remove("active");
+    newsHead.classList.add("active");
+}
+
+function OpenDel() {
+    delContainer.style.display = 'flex';
+}
+
+function CloseDel() {
+    delContainer.style.display = 'none';
+}
+
+// Init
+const InitEditForm = document.getElementById('edit-form-initiative');
+
+const InitDelContainer = document.getElementById('del-init-container');
+
+function InitOpenForm() {
+    InitEditForm.style.display = 'flex';
+}
+
+function InitCloseForm() {
+    InitEditForm.style.display = 'none';
+}
+
+function InitOpenDel() {
+    InitDelContainer.style.display = 'flex';
+}
+
+function InitCloseDel() {
+    InitDelContainer.style.display = 'none';
 }
