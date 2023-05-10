a497e99cb157266985105f396da9ab2196708cbd
Shelumiel Ediriarachchi <61077989+shelu-edi@users.noreply.github.com>
2023-05-10 17:53:36 +0530

    5/10/23

--- js/navbar.js
+++ js/navbar.js
@@ -104,3 +104,18 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+
+// Close mobile menu when clicked
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    }
+
+}
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
